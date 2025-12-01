4e1f935df0e37eca369a8dd283d75cf946a42692
Pedro Henrique Barbosa <132113357+Pedrhn@users.noreply.github.com>
2025-12-01 18:31:22 -0300

    Update otimizador.py

--- codegen/otimizador.py
+++ codegen/otimizador.py
@@ -4,11 +4,11 @@ class Otimizador:
     def __init__(self):
         self.passes = binding.PassManagerBuilder()
         self.passes.opt_level = 3
-        self.modulo_pm = binding.ModulePassManager()
-        self.passes.populate(self.modulo_pm)
+        self.module_pm = binding.ModulePassManager()
+        self.passes.populate(self.module_pm)
 
     def otimizar(self, llvm_ir):
         modulo = binding.parse_assembly(llvm_ir)
         modulo.verify()
-        self.modulo_pm.run(modulo)
+        self.module_pm.run(modulo)
         return str(modulo)
